5dc0970ada0922682ab6dcabb9f3d5c742cc3c09
Jhon-Arley-Castillo-Vitovis <jhona_castillov@soy.sena.edu.co>
2025-03-26 22:52:12 -0500

    Se inician pruebas con modelos en hugginface

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv()
 
 # Variables de entorno
 HF_API_KEY = os.getenv("HF_API_KEY")  # Token de Hugging Face
+HF_MODELO_LLM = os.getenv("HF_MODELO_LLM")
 HF_MODELO_TTS = os.getenv("HF_MODELO_TTS")
-HF_MODELO_CAPTION = os.getenv("HF_MODELO_CAPTION")
+HF_MODELO_STT = os.getenv("HF_MODELO_STT")
+HF_MODELO_IMG = os.getenv("HF_MODELO_IMG")
